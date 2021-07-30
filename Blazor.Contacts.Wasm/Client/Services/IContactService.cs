37343386002de691752a6b22ad5709ed5f7777ff
Willy Gomez <wp_gomez@hotmail.com>
2021-07-30 17:30:05 -0500

    nuevo

---
+++ Blazor.Contacts.Wasm/Client/Services/IContactService.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.Contacts.Wasm.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Contacts.Wasm.Client.Services
+{
+    public interface IContactService
+    {
+        Task SaveContact(Contact contact);
+        Task DeleteContact(int id);
+        Task<IEnumerable<Contact>> GetAll();
+        Task<Contact> GetDetails(int id);
+    }
+}
